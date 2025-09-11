7944625ddfb76403b3ed2aee7bd287555083b2ab
vikas jogi <jogivikas666@gmail.com>
2025-09-11 20:57:20 +0530

    delete

--- react4/todo/src/Todo.jsx
+++ react4/todo/src/Todo.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
+export default function Todo() {
+  let [todo, setTodo] = useState([]);
+  let [newTodo, setNewTodo] = useState("");
 
+  let addNewTask = () => {
+    if (newTodo.trim() === "") return; // prevent empty todos
+    setTodo((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4() }];
+    });
+    setNewTodo("");
+  };
 
-export default function Todo()
-{
+  let uploadTodoValue = (event) => {
+    setNewTodo(event.target.value);
+  };
 
-    return (
-        <>
+  let deleteTodo = (id) => {
+    let copy = todo.filter((t) => t.id !== id); // ✅ fixed
+    setTodo(copy);
+  };
 
-
-     <h1>Todo List</h1>
-
-        </>
-    )
+  return (
+    <div>
+      <input
+        placeholder="add todo"
+        value={newTodo}
+        onChange={uploadTodoValue}
+      />
+      <br />
+      <button onClick={addNewTask}>add</button>
+      <br />
+      <h1>Todo List</h1>
+      <ul>
+        {todo.map((t) => (
+          <li key={t.id}>
+            <span>{t.task}</span> &nbsp;
+            <button onClick={() => deleteTodo(t.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
