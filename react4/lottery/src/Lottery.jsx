96054efd134df95cd0196b7bdf5f5be3ad68728d
vikas jogi <jogivikas666@gmail.com>
2025-09-16 13:13:07 +0530

    new componet

--- react4/lottery/src/Lottery.jsx
+++ react4/lottery/src/Lottery.jsx
@@ -11,12 +11,14 @@ let iswinning=sum(ticket)===15 ;
 
             <div className="lottery">
                 <h3>generate Ticket</h3>
+                <h1>hi</h1>
                 
              <span>{ticket[0]}</span>
              <span>{ticket[1]}</span>
              <span>{ticket[2]}</span>
 
             </div>
+            <button onClick={()=>setTicket(helper(3))}>BuyTicket</button>
             <h3>{iswinning?"You Win":"You Lose"}</h3>
         </div>
     );
